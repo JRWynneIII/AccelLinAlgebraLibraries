7b96f589473cd2163df5b746e7d980f8f2af4b32
James Riley Wynne III <wynnejr@ornl.gov>
2014-03-17 15:46:04 -0400

    Added pedagogical comments in the magma source files

--- MAGMA/magma.f90
+++ MAGMA/magma.f90
@@ -1,3 +1,7 @@
+! This module is used to provide FORTRAN interfaces to the C functions and
+! routines provided by MAGMA. To accomplish this, the ISO_C_BINDING module needs
+! to be included so that all the variables are the right types for the C
+! functions.
 module magma
   Interface
       Integer function magma_sgesv(n, nrhs, A, ida, ipiv,B, idb, info) BIND (C, NAME="magma_sgesv")
--- MAGMA/sgesv.f90
+++ MAGMA/sgesv.f90
@@ -1,12 +1,17 @@
 Program LinearEquations
-! solving the matrix equation A*x=b using LAPACK
+! This solves the matrix equation A*x=b using MAGMA
+! Include both the custom magma module (magma.f90) and the ISO_C_BINDING module
+! so that the variables that are passed to the magma function are of the right C
+! type
   use magma
   use iso_c_binding
   Implicit none
-! declarations, notice single precision
+
+! Declarations, notice single precision
   Real (KIND=C_FLOAT) :: A(3,3), b(3)
-  integer (KIND=C_INT) :: i, j, pivot(3), ok, status
-! define matrix A
+  Integer (KIND=C_INT) :: i, j, pivot(3), ok, status
+
+! Define matrix A
   A(1,1)=3.1
   A(1,2)=1.3
   A(1,3)=-5.7
@@ -16,22 +21,22 @@ Program LinearEquations
   A(3,1)=3.4
   A(3,2)=7.2
   A(3,3)=-8.8
-! define vector b, make b a matrix and you can solve multiple
+
+! Define vector b, make b a matrix and you can solve multiple
 ! equations with the same A but different b
   b(1)=-1.3
   b(2)=-0.1
   b(3)=1.8
-! find the solution using the LAPACK routine SGESV
-! magma_sgesv(int n, int, nrhs, float* A, int Ida, int* ipiv, float* B, int idb,
-! int* info)
+
+! Find the solution using the MAGMA routine MAGMA_SGESV
   status = MAGMA_SGESV(3, 1, A, 3, pivot, b, 3, ok)
-!
-! parameters in the order as they appear in the function call
+
+! Parameters in the order as they appear in the function call
 !    order of matrix A, number of right hand sides (b), matrix A,
 !    leading dimension of A, array that records pivoting, 
 !    result vector b on entry, x on exit, leading dimension of b
 !    return value   
-! 
+
 ! print the vector x
   do i=1, 3
      write(*,*) b(i)
