95c19fc45d718ca1ce55c97881e68fa7fafbb7dc
James Riley Wynne III <wynnejr@ornl.gov>
2014-03-17 15:51:32 -0400

    Added pedagogical comments to CULA source files

--- CULA/LinEq_CULA.f90
+++ CULA/LinEq_CULA.f90
@@ -1,14 +1,16 @@
 Program LinearEquations
+! Include the cula_lapack module so that you have access to all the CULA
+! interfaces defined there
   use cula_lapack
-! solving the matrix equation A*x=b using cula
+! This code solves the matrix equation A*x=b using cula
   Implicit none
 
-! declarations, notice single precision
+! Declarations, notice single precision
   Real*4 A(3,3), b(3)
   integer i, j, pivot(3), ok
   integer status
 
-! define matrix A
+! Define matrix A
   A(1,1)=3.1
   A(1,2)=1.3
   A(1,3)=-5.7
@@ -18,24 +20,27 @@ Program LinearEquations
   A(3,1)=3.4
   A(3,2)=7.2
   A(3,3)=-8.8
-! define vector b, make b a matrix and you can solve multiple
+
+! Define vector b, make b a matrix and you can solve multiple
 ! equations with the same A but different b
   b(1)=-1.3
   b(2)=-0.1
   b(3)=1.8
+
 ! Initialize the Accelerator/Start a CULA instance
   status = CULA_INITIALIZE()
-! find the solution using the CULA routine SGESV
+! Find the solution using the CULA routine SGESV
   status = CULA_SGESV(3, 1, A, 3, pivot, b, 3)
 !
-! parameters in the order as they appear in the function call
+! Parameters in the order as they appear in the function call
 !    order of matrix A, number of right hand sides (b), matrix A,
 !    leading dimension of A, array that records pivoting, 
 !    result vector b on entry, x on exit, leading dimension of b
 !    return value   
 ! 
-! shutdown the CULA instance
+! Shutdown the CULA instance
   status = CULA_SHUTDOWN()
+
 ! write(*,*) the vector x
   do i=1, 3
      write(*,*) b(i)
